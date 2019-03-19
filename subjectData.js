015a906d66392fee5069f1892b7e0c67dc72fbf3
Matthew Mack <matthew.y.p.mack@gmail.com>
2019-03-19 15:08:52 +1100

    added subjectData.js - to be filled with a few more subjects

---
+++ subjectData.js
@@ -0,0 +1,32 @@
+subjectData = {
+  "level1Subjects": [
+    {
+      "id": "MAST10006",
+      "name": "Calculus 2",
+      "periodsOffered": ["S1", "S2"],
+      "subjectPrereqs": [["MAST10005", "MAST10007"]],
+      "nonsubjectPrereqs": "",
+      "nonAllowedSubjects": ["MAST10006", "MAST10009", "MAST10019"]
+    },
+    {
+      "id": "MAST10007",
+      "name": "Linear Algebra",
+      "periodsOffered": ["S1", "S2", "Summer"],
+      "subjectPrereqs": [["MAST10005", "MAST10006"]],
+      "nonsubjectPrereqs": "Study score of at least 27 in VCE Specialist Mathematics 3/4 or equivalent",
+      "nonAllowedSubjects": ""
+    },
+  ],
+  "level2Subjects": [
+    {
+      "id": "MAST10009",
+      "name": "Vector Calculus",
+      "periodsOffered": ["S1", "S2"],
+      "subjectPrereqs": [["MAST10006", "MAST10009", "MAST10019"],["MAST10007", "MAST10008"]],
+      "nonsubjectPrereqs": "",
+      "nonAllowedSubjects": ""
+    }
+  ],
+  "level3Subjects": [],
+  "postgradSubjects": []
+}
